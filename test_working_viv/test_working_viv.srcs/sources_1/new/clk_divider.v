d68ddd09ec0140fae5d6ee4b193bc023b6cc2ca8
Jess237 <rojo_@LAPTOP-APNPFDQU.(none)>
2023-11-30 20:29:35 -0800

    fixed clock divide module

--- test_working_viv/test_working_viv.srcs/sources_1/new/clk_divider.v
+++ test_working_viv/test_working_viv.srcs/sources_1/new/clk_divider.v
@@ -8,20 +8,23 @@ module clk_divider(
 
     reg [1:0] divider=2'b00;
     reg toggle=1'b1;
-    reg [7:0] sample_counter_temp;
-    always @(posedge clk) begin
+    //reg [7:0] sample_counter_temp;
+    always @(posedge clk or negedge rst) begin
         if(~rst) begin
-        // Increment the div_counter when the divided clock toggles
-        if (toggle==1'b1) sample_counter_temp <= sample_counter_temp + 1;
-        
-        // Generate a divided clock signal
-        if (divider == 2'b00) begin
-            toggle <= ~toggle;
-            divider <= 2'b01;
-        end 
-        else divider <= divider + 1; 
-    end
-        else sample_counter_temp<=0;
+            // Increment the div_counter when the divided clock toggles
+            //if (toggle==1'b1) sample_counter_temp <= sample_counter_temp + 1; 
+            // Generate a divided clock signal
+            if (divider == 2'b00) begin
+                toggle <= ~toggle;
+                divider <= 2'b01;
+            end 
+            else divider <= divider + 1; 
+        end
+        else begin
+            //sample_counter_temp<=0;
+            toggle<=0;
+            //toggle<=~toggle;
+        end
     end
 assign div_counter=toggle; //div_counter_out is 4 sys clock counts
 //assign div_counter=sample_counter_temp;

fe6b62525db95d2a04f767fda159efd47466b6ae
Jess237 <rojo_@LAPTOP-APNPFDQU.(none)>
2023-11-30 23:23:00 -0800

    Working : counter that counts high samples. Subsampling clk. Need to switchsubsampling clock with actual within pulse_generator to meet Nyquist criteria..... To compute RMS accurately but the amount of bits and registers used for that computation might be quite large....This is a tomorrow problem

--- test_working_viv/test_working_viv.sim/sim_1/behav/xsim/xsim.dir/pulse_generator_tb_behav/obj/xsim_1.c
+++ test_working_viv/test_working_viv.sim/sim_1/behav/xsim/xsim.dir/pulse_generator_tb_behav/obj/xsim_1.c
@@ -62,7 +62,7 @@ IKI_DLLESPEC extern void execute_23(char*, char *);
 IKI_DLLESPEC extern void execute_24(char*, char *);
 IKI_DLLESPEC extern void execute_6(char*, char *);
 IKI_DLLESPEC extern void execute_7(char*, char *);
-IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_no_reg(char*, char*, char*);
+IKI_DLLESPEC extern void execute_16(char*, char *);
 IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
 IKI_DLLESPEC extern void execute_18(char*, char *);
 IKI_DLLESPEC extern void execute_19(char*, char *);
@@ -79,14 +79,12 @@ IKI_DLLESPEC extern void execute_28(char*, char *);
 IKI_DLLESPEC extern void execute_29(char*, char *);
 IKI_DLLESPEC extern void execute_30(char*, char *);
 IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-IKI_DLLESPEC extern void transaction_9(char*, char*, unsigned, unsigned, unsigned);
-IKI_DLLESPEC extern void transaction_14(char*, char*, unsigned, unsigned, unsigned);
-funcp funcTab[27] = {(funcp)execute_8, (funcp)execute_9, (funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)execute_24, (funcp)execute_6, (funcp)execute_7, (funcp)vlog_simple_process_execute_0_fast_no_reg, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_18, (funcp)execute_19, (funcp)execute_4, (funcp)execute_5, (funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_28, (funcp)execute_29, (funcp)execute_30, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_9, (funcp)transaction_14};
-const int NumRelocateId= 27;
+funcp funcTab[25] = {(funcp)execute_8, (funcp)execute_9, (funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)execute_24, (funcp)execute_6, (funcp)execute_7, (funcp)execute_16, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_18, (funcp)execute_19, (funcp)execute_4, (funcp)execute_5, (funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_28, (funcp)execute_29, (funcp)execute_30, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 25;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/pulse_generator_tb_behav/xsim.reloc",  (void **)funcTab, 27);
+	iki_relocate(dp, "xsim.dir/pulse_generator_tb_behav/xsim.reloc",  (void **)funcTab, 25);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }

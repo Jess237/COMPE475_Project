1c22d61ed2390da19e5671705e9c73d87215c6c7
Jess237 <rojo_@LAPTOP-APNPFDQU.(none)>
2023-12-03 02:23:55 -0800

    created pwm top module and added constraint file. Committing prior to bitstream generation

---
+++ test_working_viv/test_working_viv.srcs/sources_1/new/PWM_Generator.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+module PWM_Generator(
+    input clk,
+    input rst,
+    input sw,
+    output wire JA1
+    );
+
+
+    wire [15:0] number_of_samples_logic_high;
+    wire polling_complete_flag_g;
+    wire [15:0] number_of_samples;
+    wire [14:0] width_sig;//for testing only
+
+    //reg [1:0] pwm_en_state;
+
+	// Instantiate the Unit Under Test (UUT)
+	pulse_generator uut (
+        .clk(clk), 
+		.rst(rst), 
+		.sw(sw),
+        .number_of_samples_logic_high(number_of_samples_logic_high), //1111+1=0
+        .JA1(JA1),
+        .polling_complete_flag_g(polling_complete_flag_g),
+        .number_of_samples(number_of_samples),
+        .width_sig(width_sig)
+        );
+
+    //case
+endmodule
+   
+    
+endmodule

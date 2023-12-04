1213ab469489331cd6b4054f3abd430eceb1b640
Jess237 <rojo_@LAPTOP-APNPFDQU.(none)>
2023-12-03 18:17:16 -0800

    new project for hw implementation

---
+++ PWM_HW/PWM_HW.srcs/sources_1/new/PWM_Generator.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module PWM_Generator(
+    input clk,
+    input rst,
+    input [3:0] sw,
+    output wire JA1
+    );
+
+    wire [15:0] number_of_samples_logic_high;
+    wire polling_complete_flag_g;
+    wire [15:0] number_of_samples;
+    wire [14:0] width_sig;//for testing only
+
+    //reg [1:0] pwm_en_state;
+
+	// Instantiate the Unit Under Test (UUT)
+	pulse_generator uut (
+        .clk(clk), 
+		.rst(rst), 
+		.sw(sw),
+        .number_of_samples_logic_high(number_of_samples_logic_high), 
+        .polling_complete_flag_g(polling_complete_flag_g),
+        .number_of_samples(number_of_samples),
+        .width_sig(width_sig),
+        .pwm(JA1)
+        );
+//assign JA_1 = JA[0];
+    //case
+   
+    
+endmodule
